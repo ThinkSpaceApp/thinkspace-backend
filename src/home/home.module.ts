dd6de8d72850b114fcab1c5506fcf25a60aed9c9
Monteiro <guilhermemr2011j3@gmail.com>
2025-06-15 16:15:29 -0300

    Update home.module.ts

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
 import { HomeController } from "./home.controller";
+import { UsersModule } from "../users/users.module";
+import { PrismaModule } from "../prisma/prisma.module";
 
-@Module({
+@Module({  
+  imports: [UsersModule, PrismaModule],
   controllers: [HomeController],
 })
 export class HomeModule {}
