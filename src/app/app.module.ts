e8c29e75b8312def4f07b2f2918861384a6556f8
Monteiro <guilhermemr2011j3@gmail.com>
2025-06-15 13:29:47 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "../prisma/prisma.module";
 import { AuthModule } from "../auth/auth.module";
-import { HomeModule } from "../auth/auth.module";
+import { HomeModule } from "../home/home.module";
 import { MateriaisModule } from "../materiais/materiais.module";
 import { AppController } from "./app.controller";
 
