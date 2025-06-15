5b7866bb8bfe1863c23d169765e37bb129ed6520
Monteiro <guilhermemr2011j3@gmail.com>
2025-06-14 22:48:27 -0300

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -123,7 +123,6 @@ export class AuthController {
 
     const cookieOptions: CookieOptions = {
       httpOnly: true,
-      domain: ".thinkspace.app.br",
       secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
