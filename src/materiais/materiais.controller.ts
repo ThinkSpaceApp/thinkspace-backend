0d16a330e28e987b7a06dfd7e1d911ced46bf14b
Monteiro <guilhermemr2011j3@gmail.com>
2025-06-18 17:14:28 -0300

    Update materiais.controller.ts

--- src/materiais/materiais.controller.ts
+++ src/materiais/materiais.controller.ts
@@ -27,6 +27,11 @@ export class MateriaisController {
   constructor(private readonly materiaisService: MateriaisService) {}
 
   @Get("/")
+  getStatus() {
+    return { status: "Servidor de materiais ativo!" };
+  }
+  
+  @Get()
   async listarMateriais(@Req() req: Request) {
     const materiais = await this.materiaisService.listarPorUsuario((req.user as any).userId);
     return {
