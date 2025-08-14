88d6a3076b26e257e7492d284150aed7daf6c938
Maria Eduarda Machado <mariaeduardamachadomendes@gmail.com>
2025-08-14 18:59:05 -0300

    teste multer 2

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ const SWAGGER_PASS = process.env.SWAGGER_PASS;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const bodyParser = require('body-parser');
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   const expressApp = app.getHttpAdapter().getInstance();
   expressApp.set("trust proxy", 1);
