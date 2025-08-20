ae2a53faeeccd5065887ac8d2678e20876b1ca3f
Maria Eduarda Machado <mariaeduardamachadomendes@gmail.com>
2025-08-20 18:40:35 -0300

    chore: metricas semanais ajustes 3

--- src/metricas/metricas.controller.ts
+++ src/metricas/metricas.controller.ts
@@ -5,15 +5,15 @@ import { MetricasService } from './metricas.service';
 export class MetricasController {
   constructor(private readonly metricasService: MetricasService) {}
 
+  @Get('ranking')
+  async getRanking() {
+    return await this.metricasService.getRanking();
+  }
+
   @Get(':userId')
   async getMetricas(@Param('userId') userId: string, @Query('weeksAgo') weeksAgo?: string) {
     const weeks = weeksAgo ? Number(weeksAgo) : 0;
     return await this.metricasService.getMetricasAluno(userId, weeks);
-  }
-
-  @Get('ranking')
-  async getRanking() {
-    return await this.metricasService.getRanking();
   }
 
   @Get(':userId/melhores-materias')
