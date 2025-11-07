cce8f3eeda2416049dc559c23e5043d906d27139
Maria Eduarda Machado <mariaeduardamachadomendes@gmail.com>
2025-11-07 15:29:46 -0300

    fix: nest erro

--- src/notificacoes/notificacoes.module.ts
+++ src/notificacoes/notificacoes.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { NotificacoesController } from './notificacoes.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+	imports: [PrismaModule],
 	controllers: [NotificacoesController],
 	providers: [],
 })
